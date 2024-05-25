aec5107de24bf966356f054044d2cd1e0e454f94
lemdorot <lemdorot@gmail.com>
2024-05-25 16:44:29 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,29 @@
+import { userEvent } from '@storybook/testing-library';
+import { fireEvent, screen } from '@testing-library/react';
+import { componentRouter } from 'shared/lib/tests/componentRender/componentRender';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+	test('test render', () => {
+		componentRouter(<Counter />, {
+			initialState: { counter: { value: 10 } },
+		});
+		expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+	});
+
+	test('increment', () => {
+		componentRouter(<Counter />, {
+			initialState: { counter: { value: 10 } },
+		});
+		userEvent.click(screen.getByTestId('increment-btn'));
+		expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+	});
+
+	test('decrement', () => {
+		componentRouter(<Counter />, {
+			initialState: { counter: { value: 10 } },
+		});
+		userEvent.click(screen.getByTestId('decrement-btn'));
+		expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+	});
+});
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -2,22 +2,28 @@ import { I18nextProvider } from 'react-i18next';
 import React, { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import i18nForTests from '../../../config/i18n/i18nForTests';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
 
 export interface componentRouterOptions {
 	route?: string;
+	initialState?: DeepPartial<StateSchema>;
 }
 
 export function componentRouter(component: ReactNode, options: componentRouterOptions = {}) {
 	const {
 		route = '/',
+		initialState,
 	} = options;
 
 	return render(
-		<MemoryRouter initialEntries={[route]}>
-			<I18nextProvider i18n={i18nForTests}>
-				{component}
-			</I18nextProvider>
-		</MemoryRouter>,
+		<StoreProvider initialState={initialState}>
+			<MemoryRouter initialEntries={[route]}>
+				<I18nextProvider i18n={i18nForTests}>
+					{component}
+				</I18nextProvider>
+			</MemoryRouter>
+		</StoreProvider>,
 	);
 }
