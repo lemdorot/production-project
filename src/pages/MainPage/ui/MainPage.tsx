169f8e4c716b8c1faafdc71ce727749e75651fc6
lemdorot <lemdorot@gmail.com>
2024-05-26 00:03:43 +0300

    add new input, add login form ui

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
 	const { t } = useTranslation('main');
+	const [value, setValue] = useState('');
+
+	const onChange = (val: string) => {
+		setValue(val);
+	};
 
 	return (
 		<div>
