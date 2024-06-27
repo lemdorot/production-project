1a98f980fe58b20561bcc87da9d84b7d2336453b
lemdorot <lemdorot@gmail.com>
2024-06-27 22:26:07 +0300

    add dropdown, update navbar, add avatar with menu

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ListBox } from 'shared/ui/ListBox/ListBox';
-import { HStack } from 'shared/ui/Stack';
 import { Page } from 'widgets/Page/Page';
 
 const MainPage = () => {
@@ -15,25 +13,6 @@ const MainPage = () => {
 	return (
 		<Page>
 			{t('Главная страница')}
-			<div>sdfsafd</div>
-			<HStack>
-				<div>sdfsafd</div>
-				<ListBox
-					defaultValue="Выберите значение"
-					onChange={(value: string) => {}}
-					value={undefined}
-					items={[
-						{ value: '1', content: '123' },
-						{ value: '12', content: 'asf', disabled: true },
-						{ value: '13', content: 'asf' },
-					]}
-				/>
-			</HStack>
-			<div>sdfsafd</div>
-			<div>sdfsafd</div>
-			<div>sdfsafd</div>
-			<div>sdfsafd</div>
-			<div>sdfsafd</div>
 		</Page>
 	);
 };
--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -7,10 +7,58 @@ export default {
 	argTypes: {
 		backgroundColor: { control: 'color' },
 	},
+	decorators: [
+		(Story) => <div style={{ padding: 100 }}><Story /></div>,
+	],
 } as ComponentMeta < typeof ListBox>;
 
-const Template: ComponentStory<typeof ListBox> = () => <ListBox onChange={(value: string) => {}} />;
+const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
+	value: '123',
+	items: [
+		{ content: 'dghjsdfghsfg123', value: '123' },
+		{ content: 'dghjsasdfasddfghsfg123', value: '12343' },
+	],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+	direction: 'top left',
+	value: '123',
+	items: [
+		{ content: 'dghjsdfghsfg123', value: '123' },
+		{ content: 'dghjsasdfasddfghsfg123', value: '12343' },
+	],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+	direction: 'top right',
+	value: '123',
+	items: [
+		{ content: 'dghjsdfghsfg123', value: '123' },
+		{ content: 'dghjsasdfasddfghsfg123', value: '12343' },
+	],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+	direction: 'bottom left',
+	value: '123',
+	items: [
+		{ content: 'dghjsdfghsfg123', value: '123' },
+		{ content: 'dghjsasdfasddfghsfg123', value: '12343' },
+	],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+	direction: 'bottom right',
+	value: '123',
+	items: [
+		{ content: 'dghjsdfghsfg123', value: '123' },
+		{ content: 'dghjsasdfasddfghsfg123', value: '12343' },
+	],
 };
